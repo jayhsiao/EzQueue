88cf236a14c043ae22365455117ddf199445d3b5
v-jayhsiao <v-jayhsiao@catchplay.com>
2016-09-11 23:35:22 +0800

    commit

--- src/main/webapp/js/facebook.js
+++ src/main/webapp/js/facebook.js
@@ -1,6 +1,6 @@
 window.fbAsyncInit = function() {
 	FB.init({
-		appId : '592070890950054',
+		appId : '554860634671080',
 		cookie : true, // enable cookies to allow the server to access
 		// the session
 		xfbml : true, // parse social plugins on this page
